6a407d6310b08066b7db45bfafaf5ac3a91e9e7c
Kartik <k03agrawal@gmail.com>
2025-03-31 17:30:31 +0530

    custom react

--- customreact/customreact.js
+++ customreact/customreact.js
@@ -1,10 +1,23 @@
 function customRender(reactElement, container){
-    const domElement = document.createElement(reactElement.type)
+    /*const domElement = document.createElement(reactElement.type)
     domElement.innerHTML = reactElement.children
     domElement.setAttribute('href',reactElement.props.href)
     domElement.setAttribute('target', reactElement.props.target)
 
+    container.appendChild(domElement)*/
+
+    // ab humlog isse accha code likheneg that will be loop based 
+    // toh adha kaam toh pehlke jaisa hi hoga
+
+    const domElement = document.createElement(reactElement.type)
+    domElement.innerHTML = reactElement.children
+    for (const prop in reactElement.props) {
+        if(prop === 'children') continue;
+        domElement.setAttribute(prop, reactElement.props[prop])
+    }
     container.appendChild(domElement)
+
+    //bsa yehi hai apna custom react
 
 }
 
@@ -19,6 +32,6 @@ const reactElement ={
 }
 
 const maincontainer = document.querySelector('#root')
-
+// root ko grab kiya humne 
 
 customRender(reactElement, maincontainer)
